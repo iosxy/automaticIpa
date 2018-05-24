b6ae9d4acf942b558ab9c9e31e06a1afc44e382a
iosxy <ios@sina.com>
2018-05-24 14:11:43 +0800

    no message

---
+++ ipaDemo/ipaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipaDemo
+//
+//  Created by 欢瑞世纪 on 2018/5/24.
+//  Copyright © 2018年 欢瑞世纪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
